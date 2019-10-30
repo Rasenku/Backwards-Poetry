4d3b97bb851813e14577580a99f0c989c129067e
Dahveyea <dahveyea.cowan@gmail.com>
2019-10-29 19:09:13 -0700

    first commit

---
+++ poetry.py
@@ -0,0 +1,24 @@
+poem = 'If you are a dreamer, come in,
+If you are a dreamer, a wisher, a liar,
+A hope-er, a pray-er, a magic bean buyer…
+If you’re a pretender, come sit by my fire
+For we have some flax-golden tales to spin.
+Come in!
+Come in!'
+
+
+#TODO: get a list of strings that contains lines of poem
+
+#Use lines = poem.split("\n")
+def lines_printed_backwards():
+    '''This function takes in a list of strings containing the lines of your poem as arguments and will print the poem lines out in reverse with the line numbers reversed. For example, if you poem is Invitation by Shel Silverstein.'''
+
+def my_own_rearrange():
+    pass
+
+def lines_printed_random():
+
+    pass
+
+#Testingcode
+#lines_printed_backwards(lines)
