32c9cd50ef0a10ed97066c27d3a2bf501d3f8b57
Dahveyea <dahveyea.cowan@gmail.com>
2019-11-08 20:57:41 -0800

    Added a a different poetry

--- poetry.py
+++ poetry.py
@@ -7,9 +7,42 @@ def reverse(s):
         print(f"{index} {line}")
         index -= 1
 
-s = '''A winter blanket
-Covers the Earth in respose
-but only a dream''' .split("\n")
+s = '''The wind whistles past my ears.
+Closing my eyes, I lose all my fears.
+The waves crash into the rocks.
+Out here there is no time on my clock.
+The serenity I feel here just soothes my mind.
+A peaceful day so hard to find.
+The breeze just calms my soul.
+Helps me think about what is my life's goal.
+I then look out over the ocean,
+And it feels like my life has lost its commotion.
+The sun sets down over the clouds.
+But the orange glow around makes me proud.
+As the night draws near.
+I feel like where I need to be is here.
+The soothing nature this afternoon brings
+Just feels like such a beautiful thing.
+I sit and wonder where life will go,
+But right now all I want is for time to slow.
+To enjoy this moment and feel free,
+To clear my mind and find some glee.
+It's days like these I truly treasure.
+Amazing nights and beautiful weather.
+It may not seem like much.
+But it's moments like these I want to clutch.
+For once I feel like life is bliss.
+So many hard days in which my happiness was missed.
+I could get lost listening to the waves.
+Listening to the birds and watching how the clouds behave.
+I could close my eyes and fade into the night.
+The tranquility I feel helps me win the fight.
+As the waves keep crashing into the rocks,
+I feel the happiness in my heart become unlocked.
+The day is drawing to a close.
+The peacefulness I feel right now I'll only know.
+Right now my mind is finally clear.
+It's time to go as the night draws near.''' .split("\n")
 reverse(s)
 
 def lines_printed_random(s):
@@ -24,9 +57,42 @@ def lines_printed_random(s):
 
 
 def unique_lines(s):
-    s = '''A winter blanket
-Covers the Earth in respose
-but only a dream'''
+    s = '''The wind whistles past my ears.
+Closing my eyes, I lose all my fears.
+The waves crash into the rocks.
+Out here there is no time on my clock.
+The serenity I feel here just soothes my mind.
+A peaceful day so hard to find.
+The breeze just calms my soul.
+Helps me think about what is my life's goal.
+I then look out over the ocean,
+And it feels like my life has lost its commotion.
+The sun sets down over the clouds.
+But the orange glow around makes me proud.
+As the night draws near.
+I feel like where I need to be is here.
+The soothing nature this afternoon brings
+Just feels like such a beautiful thing.
+I sit and wonder where life will go,
+But right now all I want is for time to slow.
+To enjoy this moment and feel free,
+To clear my mind and find some glee.
+It's days like these I truly treasure.
+Amazing nights and beautiful weather.
+It may not seem like much.
+But it's moments like these I want to clutch.
+For once I feel like life is bliss.
+So many hard days in which my happiness was missed.
+I could get lost listening to the waves.
+Listening to the birds and watching how the clouds behave.
+I could close my eyes and fade into the night.
+The tranquility I feel helps me win the fight.
+As the waves keep crashing into the rocks,
+I feel the happiness in my heart become unlocked.
+The day is drawing to a close.
+The peacefulness I feel right now I'll only know.
+Right now my mind is finally clear.
+It's time to go as the night draws near.'''
     print (s.upper())
 
 
